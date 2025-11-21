4ad090c80ed62e4c0f0fe5373ef9fb9aa183f53f
Sara Lambert <lambert8@illinois.edu>
2025-11-21 15:23:16 -0600

    feat: added PM2.5 and NO2 overlay options - separate & combined

--- src/components/Map/MapSection.js
+++ src/components/Map/MapSection.js
@@ -359,7 +359,7 @@ function MapSection({ setViewStateFn = () => {}, bounds, geoids = [], showSearch
 
   const COLOR_SCALE = (x) =>
     scaleColor(x, mapParams.bins, mapParams.colorScale);
-  
+
   const isVisible = (feature, filters) => {
     for (const property in filters) {
       if (typeof filters[property][0] === "string") {
@@ -555,6 +555,23 @@ function MapSection({ setViewStateFn = () => {}, bounds, geoids = [], showSearch
         // If mapping of colors, choose color based on symbolProp
         const { symbolProp } = parsedOverlay;
         const symbolKey = feature.properties[symbolProp];
+
+        // Treat as boolean map if symbolKey is not a string
+        if (typeof symbolKey === 'object') {
+          const boolMap = symbolKey;
+          const colorsToAvg = Object.keys(colors)
+            .filter(key => boolMap[key] === true);
+          if (!colorsToAvg?.length) {
+            console.error(`Failed to parse colors for ${parsedOverlay?.id}: ${JSON.stringify(symbolKey)}`, feature)
+            return colors[symbolKey];
+          }
+
+          // Average the colors together
+          return colorsToAvg.map(key => colors[key])
+            .reduce((a, c) => [a[0]+c[0], a[1]+c[1], a[2]+c[2]], [0,0,0])
+            .map(rgb => rgb / colorsToAvg.length);
+        }
+        
         return colors[symbolKey];
       },
 
@@ -579,7 +596,7 @@ function MapSection({ setViewStateFn = () => {}, bounds, geoids = [], showSearch
       beforeId: "state-label",
     })
   });
-  
+
   baseLayers.push(
     new GeoJsonLayer({
       id: "sensors",
