32e0a376f9371a6164f3124fd9f76b7872780bcf
Sara Lambert <lambert8@illinois.edu>
2026-02-18 17:44:28 -0600

    style: use correct lastUpdated icon color

--- src/components/Map/DataPanel.js
+++ src/components/Map/DataPanel.js
@@ -472,7 +472,7 @@ const DataPanel = ({ handleGeocoder }) => {
 
         {selectedSensors?.length === 0 && <div style={{ margin: '0.5rem 0' }}>
             <span style={{ fontWeight: 200, fontFamily: 'Space Grotesk' }}>
-              <FaHistory style={{ transform: 'scaleX(-1)', color: 'lightblue', marginRight: '0.35rem' }} />
+              <FaHistory style={{ transform: 'scaleX(-1)', color: 'rgba(0, 88, 153, 0.5)', marginRight: '0.35rem' }} />
               <span style={{ textDecoration: 'underline', textDecorationStyle: 'dotted', marginRight: '0.25rem' }}>updat{formatted?.time ? 'ed' : 'ing'}</span>
               {formatted?.time || 'Loading'}, {formatted?.date || 'Please Wait...'}
             </span>
