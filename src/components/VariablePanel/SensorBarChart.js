25ed4e5816291e622931100da7f9d33e5b56adea
Sara Lambert <lambert8@illinois.edu>
2026-02-19 04:36:05 -0600

    fix: checkpoint - appears to be working locally

--- src/components/VariablePanel/SensorBarChart.js
+++ src/components/VariablePanel/SensorBarChart.js
@@ -2,7 +2,8 @@ import {BarChart} from "@mui/x-charts/BarChart";
 import {pm2_5Ranges} from "../../config";
 
 export const SensorBarChart = ({ dataset, datasourceId, averageType }) => {
-  const chartSetting = {
+  const chartSettings = {
+    dataset,
     yAxis: [{
       disableLine: true, // Hides the main vertical line
       label: 'PM2.5 Mass Concentration (μg/m³)',
@@ -13,20 +14,43 @@ export const SensorBarChart = ({ dataset, datasourceId, averageType }) => {
         colors: pm2_5Ranges?.map(r => r.color),
       },
     }],
-    series: [{ dataKey: datasourceId, valueFormatter: (v) => `${Number(v)?.toFixed(1)} μg/m³`, minBarSize: '20px', }], // Minimum width of 10px
+    xAxis: [{ scaleType: 'band', dataKey: 'date',  tickPlacement:'middle', valueFormatter: (v) => {
+        if (averageType === 'week' || averageType === 'season') {
+          return v;
+        }
+        const isoTimestamp = v.split(' ').join('T') + 'Z';
+        const d = new Date(isoTimestamp);
+
+        // Use 'en-US' to ensure the Month/Day/Year order
+        const parts = new Intl.DateTimeFormat('en-US', {
+          hour: 'numeric',
+          minute: '2-digit',
+          hour12: true,
+          month: '2-digit',
+          day: '2-digit',
+          year: '2-digit'
+        }).formatToParts(d);
+
+        // Reconstruct to place the time before the date with a comma
+        const time = `${parts.find(p => p.type === 'hour').value}${parts.find(p => p.type === 'dayPeriod').value}`;
+        const date = `${parts.find(p => p.type === 'month').value}/${parts.find(p => p.type === 'day').value}/${parts.find(p => p.type === 'year').value}`;
+
+        if (averageType === 'hour') {
+          return time;
+        } else {
+          return date;
+        }
+      } }],
+    series: [{ dataKey: datasourceId, minBarSize: '20px', valueFormatter: (v) => `${Number(v)?.toFixed(1)} μg/m³` }], // Minimum width of 10px
     height: 200,
     margin: { left: 0 },
+    reverse: true,
   };
 
   //console.log(dataset);
   return (
     <div style={{ width: '100%' }}>
-      <BarChart
-        dataset={dataset}
-        xAxis={[{ scaleType: 'band', dataKey: 'date' }]}
-
-        {...chartSetting}
-      />
+      <BarChart {...chartSettings}  />
     </div>
   );
 }
