de715f7b5bfe712a11d48260a3a942481f40fa2a
Sara Lambert <lambert8@illinois.edu>
2026-04-04 03:34:45 -0500

    fix: raise aqi_max limit for consistent coloring

--- src/config/index.js
+++ src/config/index.js
@@ -83,5 +83,5 @@ export const pm2_5Ranges = [
   { label: 'Unhealthy for Sensitive Groups',  aqi_min: 101, aqi_max: 151,   pm25_min: 35.5, pm25_max: 55.4,     color: "rgb(220, 117, 0)", border: "rgb(222, 204, 183)", colorComponents: [220, 117, 0], borderComponents: [222, 204, 183] },
   { label: 'Unhealthy',                       aqi_min: 151, aqi_max: 201,   pm25_min: 55.5, pm25_max: 125.4,    color: "rgb(228, 0, 4)", border: "rgb(233, 187, 187)", colorComponents: [228, 0, 4], borderComponents: [233, 187, 187] },
   { label: 'Very Unhealthy',                  aqi_min: 201, aqi_max: 301,   pm25_min: 125.5, pm25_max: 225.4,   color: "rgb(130, 0, 197)", border: "rgb(207, 176, 224)", colorComponents: [130, 0, 197], borderComponents: [207, 176, 224] },
-  { label: 'Hazardous',                       aqi_min: 301, aqi_max: 10000, pm25_min: 225.5, pm25_max: 100000,  color: "rgb(139, 13, 56)", border: "rgb(227, 115, 153)", colorComponents: [139, 13, 56], borderComponents: [227, 115, 153] },
+  { label: 'Hazardous',                       aqi_min: 301, aqi_max: 100000,pm25_min: 225.5, pm25_max: 100000,  color: "rgb(139, 13, 56)", border: "rgb(227, 115, 153)", colorComponents: [139, 13, 56], borderComponents: [227, 115, 153] },
 ];
