38179b9fefc5266abc90e88862635024d8073c49
Shubham Kumar <shubhamk008@yahoo.in>
2026-04-10 03:59:40 -0500

    Added Team page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home, LoadingMessage, ErrorPage } from "./components/";
 import ParquetReaderComponent from "./components/Map/ParquetReaderComponent";
 
 const Map = lazy(() => import('./components/Pages/Map'));
+const Team = lazy(() => import('./components/Pages/Team'));
 const About = lazy(() => import('./components/Pages/About'));
 //const Guide = lazy(() => import('./components/Pages/Guide'));
 //const Community = lazy(() => import('./components/Pages/Community'));
@@ -17,6 +18,7 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/map" element={<Map />} />
+          <Route path="/team" element={<Team />} />
           <Route path="/about" element={<About />} />
           <Route path="/*" element={<ErrorPage />} />
         </Routes>
