e02f7cf3c3e99862a401e01e7b5d8a16f4dca3b9
Sara Lambert <lambert8@illinois.edu>
2026-04-13 16:47:16 -0500

    style: cleanup Navbar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -129,15 +129,11 @@ export default function Nav({
           <Grid container justifyContent={largeScreen ? 'space-between' : 'center'} alignItems={'center'} flexDirection={largeScreen ? 'row' : 'column-reverse'}>
             <Grid size={5}>
               {(largeScreen || mobileNavOpen) && <Grid container justifyContent={'space-between'} alignItems={'center'} marginBottom={'2rem'}>
-                {/* <DropdownButton style={{ fontSize: largeScreen ? '24px' : '16px' }} ButtonComponent={LButton} label={'Eng'} options={['English', 'Español']} /> */}
                 <LButton style={{ fontSize: largeScreen ? '24px' : '16px' }} onClick={() => navigate('/')}><FaHome /></LButton>
-                {/*<DropdownButton buttonProps={{size:'large'}} ButtonComponent={LButton}  label={'Maps'} />
-                <DropdownButton buttonProps={{size:'large'}} ButtonComponent={LButton}  label={'About'} />*/}
                 <LButton style={{ fontSize: largeScreen ? '24px' : '16px' }} onClick={() => navigate('/map')}>Maps</LButton>
                 <LButton style={{ fontSize: largeScreen ? '24px' : '16px' }} onClick={() => navigate('/team')}>Team</LButton>
-                {/*<LButton style={{ fontSize: largeScreen ? '24px' : '16px' }} onClick={() => navigate('/info')}>Info</LButton>*/}
+                <LButton style={{ fontSize: largeScreen ? '24px' : '16px' }} onClick={() => navigate('/resources')}>Info</LButton>
                 <LButton style={{ fontSize: largeScreen ? '24px' : '16px' }} onClick={() => navigate('/about')}>About</LButton>
-                <LButton style={{ fontSize: largeScreen ? '24px' : '16px' }} onClick={() => navigate('/resources')}>Resources</LButton>
               </Grid>}
             </Grid>
             <Grid alignItems={'end'} justifyContent={'right'} onClick={logoClicked} style={{ cursor: largeScreen ? '' : 'pointer', padding: largeScreen ? '' : '2rem 4rem' }}>
