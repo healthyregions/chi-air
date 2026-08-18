a447e070c7ef8793ba07e1a579e382176ae90022
Sara Lambert <lambert8@illinois.edu>
2026-08-18 12:42:42 -0500

    wip: try harder to clear out translation cookies when chanign locale

--- src/components/Layout/GoogleTranslate.js
+++ src/components/Layout/GoogleTranslate.js
@@ -50,12 +50,21 @@ export const GoogleTranslate = () => {
     if (locale === previous) { console.log('locale unchanged.');return; }
 
     console.log('Setting locale to: ', locale);
-    // FIXME: removeCookie does not appear to work in Safari
-    // if (!locale || locale === 'auto') {
-    //   removeCookie('googtrans', { path: '/', domain: `.${window.location.host.split(':')?.[0]}` });
-    // } else {
-      setCookie('googtrans', `/auto/${locale}`, { path: '/', domain: `.${window.location.host.split(':')?.[0]}` })
-    //}
+    if (!locale || locale === 'auto') {
+      // FIXME: removeCookie does not appear to work in Safari
+      removeCookie('googtrans', { path: '/' });
+      removeCookie('googtrans', { path: '/', domain: `${window.location.hostname}` });
+      removeCookie('googtrans', { path: '/', domain: `.${window.location.hostname}` });
+
+      // Try manually expiring the cookie
+      document.cookie = 'googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      document.cookie = 'googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=' + window.location.hostname + ';';
+      document.cookie = 'googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.' + window.location.hostname + ';';
+    } else {
+      setCookie('googtrans', `/auto/${locale}`, { path: '/' })
+      setCookie('googtrans', `/auto/${locale}`, { path: '/', domain: `${window.location.hostname}` });
+      setCookie('googtrans', `/auto/${locale}`, { path: '/', domain: `.${window.location.hostname}` });
+    }
 
     setTimeout(() => {
       window.location.reload();
